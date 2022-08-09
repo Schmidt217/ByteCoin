92fceb41c37227e3e1f25c3a237d2f0ee75bd446
Mike Schmidt <schmidt217@gmail.com>
2022-08-09 14:56:55 -0600

    starter and adding git ignore file

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Michael Schmidt on 8/8/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
+
