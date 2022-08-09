9c694b71b1950214bbb92370da7c769102d2fac1
Mike Schmidt <schmidt217@gmail.com>
2022-08-09 14:59:18 -0600

    finished project

---
+++ ByteCoin/Keys.swift
@@ -0,0 +1,15 @@
+//
+//  Key.swift
+//  ByteCoin
+//
+//  Created by Michael Schmidt on 8/9/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+struct Keys {
+    static let apiKey = "87AEB90B-32C0-4CD5-B97C-6929B4D1CD34"
+}
